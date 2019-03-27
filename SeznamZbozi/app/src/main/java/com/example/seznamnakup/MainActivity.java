a0053fc0bf02fca22d5fa3cbf0fb954fb686c94f
Tomáš Cink <cink01@student.vspj.cz>
2019-03-27 16:17:50 +0100

    main menu oprava

--- SeznamZbozi/app/src/main/java/com/example/seznamnakup/MainActivity.java
+++ SeznamZbozi/app/src/main/java/com/example/seznamnakup/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.seznamnakup;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
@@ -20,6 +23,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.menu_items,menu);
+        return true;
     }
 
     @Override
