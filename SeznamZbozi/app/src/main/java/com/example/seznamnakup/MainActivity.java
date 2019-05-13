9ee1ff182c917200edfa5b0746ea6f9ec19983e1
cink01 <43788601+cink01@users.noreply.github.com>
2019-05-13 11:38:29 +0200

    přidáno xml

--- SeznamZbozi/app/src/main/java/com/example/seznamnakup/MainActivity.java
+++ SeznamZbozi/app/src/main/java/com/example/seznamnakup/MainActivity.java
@@ -10,6 +10,13 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
@@ -20,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        parseXML();
     }
     public List<Zbozi> zbozis = Singleton.getInstance().zbozis;
     ListView lv;
@@ -64,5 +72,51 @@ public class MainActivity extends AppCompatActivity {
         ArrayAdapter<Zbozi> arrayAdapter =
                 new ArrayAdapter<Zbozi>(this, android.R.layout.simple_list_item_1, zbozis);
         lv.setAdapter(arrayAdapter);
+    }
+
+    //XML SOUBOR
+    private void parseXML() {
+        XmlPullParserFactory parserFactory;
+        try {
+            parserFactory = XmlPullParserFactory.newInstance();
+            XmlPullParser parser = parserFactory.newPullParser();
+            InputStream is = this.getResources().openRawResource(R.raw.zbozi);
+            parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
+            parser.setInput(is, null);
+            processParsing(parser);
+        } catch (XmlPullParserException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void processParsing(XmlPullParser parser) throws IOException, XmlPullParserException {
+        int eventType = parser.getEventType();
+        Zbozi tmpZbozi = null;
+
+        while (eventType != XmlPullParser.END_DOCUMENT) {
+            String eltName = null;
+
+            switch (eventType) {
+                case XmlPullParser.START_TAG:
+                    eltName = parser.getName();
+                    if ("zbozi".equals(eltName)) {
+                        tmpZbozi = new Zbozi("-", ((float) (-1.1)), ((float) (-1.1)));
+                        zbozis.add(tmpZbozi);
+                    } else if (tmpZbozi != null) {
+                        if ("nazev".equals(eltName)) {
+                            tmpZbozi.setNazev(parser.nextText());
+                        } else if ("cena".equals(eltName)) {
+                            tmpZbozi.setCena(Float.parseFloat(parser.nextText()));
+                        } else if ("pocet".equals(eltName)) {
+                            tmpZbozi.setPocet(Float.parseFloat(parser.nextText()));
+                        }
+                    }
+                    break;
+            }
+            eventType = parser.next();
+        }
+        Refresh();
     }
 }
